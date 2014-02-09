4c586d894176b7c50dade2113b750c3a8ad2329a
Espen Hovlandsdal <espen@hovlandsdal.com>
2014-02-09 17:48:30 +0100

    Whole lot of cleanup-work

---
+++ src/main/java/io/imbo/client/Url/ImboUrl.java
@@ -0,0 +1,188 @@
+/**
+ * This file is part of the imboclient-java package
+ *
+ * (c) Espen Hovlandsdal <espen@hovlandsdal.com>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+package io.imbo.client.Url;
+
+import io.imbo.client.util.TextUtils;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+/**
+ * Abstract Imbo URL for other implementations to extend
+ *
+ * @author Espen Hovlandsdal <espen@hovlandsdal.com>
+ */
+public abstract class ImboUrl implements Url {
+
+    /**
+     * Base URL
+     */
+    protected String baseUrl;
+
+    /**
+     * Public key
+     */
+    protected String publicKey;
+
+    /**
+     * Private key
+     */
+    private String privateKey;
+
+    /**
+     * Access token generator
+     */
+    private AccessToken accessToken;
+
+    /**
+     * Query parameters for the URL
+     */
+    private ArrayList<String> queryParams = new ArrayList<String>();
+
+    /**
+     * Class constructor
+     *
+     * @param baseUrl The base URL to use
+     * @param publicKey The public key to use
+     * @param privateKey The private key to use
+     */
+    public ImboUrl(String baseUrl, String publicKey, String privateKey) {
+        this.baseUrl = baseUrl;
+        this.publicKey = publicKey;
+        this.privateKey = privateKey;
+    }
+
+    /**
+     * Returns the URL with query parameters added
+     *
+     * @return Full URL with query parameters, as a String
+     */
+    public String getUrl() {
+        String url = getResourceUrl();
+        String queryString = getQueryString();
+
+        if (!queryString.isEmpty()) {
+            url += "?" + queryString;
+        }
+
+        if (publicKey == null || privateKey == null) {
+            return url;
+        }
+
+        String token = getAccessToken().generateToken(url, privateKey);
+
+        return url + (queryParams.isEmpty() ? "?" : "&") + "accessToken=" + token;
+    }
+
+    /**
+     * Get the complete URL as an URL-encoded string
+     *
+     * @return URL-encoded string
+     */
+    public String getUrlEncoded() {
+        String url = getUrl();
+        url = url.replace("&", "&amp;");
+        url = url.replace("[]", "%5B%5D");
+        return url;
+    }
+
+    /**
+     * Returns the URL with query parameters added
+     *
+     * @return Full URL with query parameters, as a String
+     */
+    public String toString() {
+        return getUrl();
+    }
+
+    /**
+     * Returns the URL in URI format
+     *
+     * @return Full URL with query parameters, as a URI
+     */
+    public URI toUri() {
+        try {
+            return new URI(getUrl());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        	return null;
+        }
+    }
+
+    /**
+     * Adds a query parameter to the URL
+     *
+     * @param key Name of the parameter. For instance "page" or "t[]"
+     * @param value Value of the parameter. For instance "10" or "border:width=50,height=50"
+     * @return URL with the query added
+     */
+    public Url addQueryParam(String key, String value) {
+        queryParams.add(key + "=" + TextUtils.urlEncode(value));
+
+        return this;
+    }
+
+    /**
+     * Resets the URL - removes all query parameters
+     *
+     * @return URL without any query parameters
+     */
+    public Url reset() {
+        queryParams.clear();
+        return this;
+    }
+
+    /**
+     * Get an instance of the access token
+     *
+     * If no instance have been provided prior to calling this method, this method must
+     * instantiate the AccessToken class and return that instance.
+     *
+     * @return AccessToken
+     */
+    public AccessToken getAccessToken() {
+        if (accessToken == null) {
+            accessToken = new AccessToken();
+        }
+
+        return accessToken;
+    }
+
+    /**
+     * Set an instance of the access token
+     *
+     * @return AccessToken $accessToken An instance of the access token
+     * @return UrlInterface
+     */
+    public Url setAccessToken(AccessToken accessToken) {
+        this.accessToken = accessToken;
+        return this;
+    }
+
+    /**
+     * Retrieves the query string for this URL
+     *
+     * @return Query string, as a String
+     */
+    private String getQueryString() {
+        if (queryParams.isEmpty()) {
+            return "";
+        }
+
+        return TextUtils.join("&", queryParams);
+    }
+
+    /**
+     * Get the raw URL (with no access token appended)
+     *
+     * @return The raw URL as a String
+     */
+    abstract protected String getResourceUrl();
+}
---
+++ src/test/java/io/imbo/client/util/TextUtilsTest.java
@@ -0,0 +1,100 @@
+/**
+ * This file is part of the imboclient-java package
+ *
+ * (c) Espen Hovlandsdal <espen@hovlandsdal.com>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+package io.imbo.client.util;
+
+import static org.junit.Assert.assertEquals;
+import io.imbo.client.util.TextUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+/**
+ * Tests for the TextUtils class
+ * 
+ * @author Espen Hovlandsdal <espen@hovlandsdal.com>
+ */
+public class TextUtilsTest {
+
+    /**
+     * Should be able to join string arrays with a single character as delimiter
+     */
+    @Test
+    public void testJoinStringArrayWithSingleCharacterAsDelimiter() {
+        String[] names = { "Christer", "Espen", "André", "Mats" };
+        assertEquals("Christer,Espen,André,Mats", TextUtils.join(",", names));
+    }
+    
+    /**
+     * Should be able to join string arrays with several characters as delimiter
+     */
+    @Test
+    public void testJoinStringArrayWithSeveralCharactersAsDelimiter() {
+        String[] names = { "Christer", "Espen", "André", "Mats" };
+        assertEquals("Christer, Espen, André, Mats", TextUtils.join(", ", names));
+    }
+
+    /**
+     * Should be able to join ArrayLists with a single character as delimiter
+     */
+    @Test
+    public void testJoinArrayListWithSingleCharacterAsDelimiter() {
+        ArrayList<String> names = new ArrayList<String>(Arrays.asList(
+            "Christer", "Espen", "André", "Mats"
+        ));
+        
+        assertEquals("Christer,Espen,André,Mats", TextUtils.join(",", names));
+    }
+    
+    /**
+     * Should be able to ArrayLists with several characters as delimiter
+     */
+    @Test
+    public void testJoinArrayListWithSeveralCharactersAsDelimiter() {
+        ArrayList<String> names = new ArrayList<String>(Arrays.asList(
+            "Christer", "Espen", "André", "Mats"
+        ));
+        assertEquals("Christer, Espen, André, Mats", TextUtils.join(", ", names));
+    }
+    
+    /**
+     * Should be able to encode more complex strings
+     */
+    @Test
+    public void testEncodingOnTransformationExample() {
+        String input = "border:color=000000,width=1,height=1";
+        assertEquals(
+            "border%3Acolor%3D000000%2Cwidth%3D1%2Cheight%3D1",
+            TextUtils.urlEncode(input)
+        );
+    }
+    
+    /**
+     * Normalizing a color should strip the hash symbol
+     */
+    @Test
+    public void testNormalizingStripsHashSymbol() {
+        assertEquals(
+            "bf1942",
+            TextUtils.normalizeColor("#bf1942")
+        );
+    }
+    
+    /**
+     * Normalizing a color should lowercase definition
+     */
+    @Test
+    public void testNormalizingLowercases() {
+        assertEquals(
+            "bf2142",
+            TextUtils.normalizeColor("#BF2142")
+        );
+    }
+}
